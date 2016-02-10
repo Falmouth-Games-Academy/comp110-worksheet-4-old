85409179344758d2cc8f96a5e2af59ab45b1995f
sw180283 <sw180283@falmouth.ac.uk>
2016-02-10 16:53:41 +0000

    Error with terminal likeness

--- PartA_TerminalHacking/TerminalHacking.cpp
+++ PartA_TerminalHacking/TerminalHacking.cpp
@@ -7,27 +7,79 @@
 const int wordLength = 5;
 const int numberOfWords = 15;
 
-char getLetter()
+/*
+char getGuess()
 {
 	while (true)
 	{
 		std::cout << "Welcome to the game. Choose a word: ";
 		std::string line;
 		std::getline(std::cin, line);
-		if (line.length() == 1 && isalpha(line[0]))
+		if (line.length() == 5 && isalpha(line[0]))
 		{
-			return
-				toupper(line[0]);
+			int i = 0;
+			while (line[i])
+			{
+				return
+					toupper(line[i]);
+				i++;
+			}
 		}
 		else
 		{
-			std::cout << "That's not a letter!" << std::endl;
+			std::cout << "That's an invalid word!" << std::endl;
+		}
+	}
+*/
+std::string getGuess()
+{
+	while (true)
+	{
+		// Get player choice word
+		std::string guess;
+		//std::string newGuess;
+		std::cout << "Guess the word: ";
+		std::getline(std::cin, guess);
+
+		if (guess.length() == 5)
+		{
+			for (int i = 0; i < guess.length(); i++)
+			{
+				if (isalpha(guess[i]))
+				{
+					guess[i] = toupper(guess[i]);
+				}
+				else
+				{
+					std::cout << "That's not a letter!" << std::endl;
+				}
+
+				return guess;
+			}
+			/*
+			for each (char c in guess)
+			{
+				newGuess.append(.append(toupper(c));
+				
+				if (!isalpha(c))
+				{
+					std::cout << "That's not a letter!" << std::endl;
+				}
+				
+			}
+			return newGuess;
+			*/
+		}
+		else
+		{
+			std::cout << "That's not 5 letters!" << std::endl;
 		}
 	}
 }
 
 int main()
 {
+
 	// Seed the random number generator with the current time,
 	// to ensure different results each time the program is run
 	srand(static_cast<unsigned int>(time(nullptr)));
@@ -60,48 +112,77 @@ int main()
 
 	// TODO: implement the rest of the game
 
-	std::string partialWord(secret.length(), '-');
+	//std::string partialWord(secret.length(), '-');
+	std::string attemptWord(secret.length(), '-');
 
 	int lives = 3;
 	char symbol = '*';
+	int l = 0;
 	//std::string livesRemaining(lives, symbol);
 
+	//bool gameOver = false;
+	//while (!gameOver)
 	while (true)
 	{
-		std::cout << partialWord << std::endl;
-		std::cout << "Attempts remaining: " << std::string(lives, symbol) << std::endl;
 
-		char letter = getLetter();
-		std::cout << "You entered " << letter << std::endl;
+		//std::cout << partialWord << std::endl;
+		std::cout << attemptWord << std::endl;
+
+		// make a string from the guess
+		std::string guess = getGuess();
+
+		std::cout << "You entered " << guess << std::endl;
+		std::cout << "Attempts remaining: " << std::string(lives, symbol) << std::endl;
+		std::cout << "Likeness: " << l << std::endl;
 
 		bool found = false;
+		//bool similar = false;
+
 		for (int i = 0; i < secret.length(); i++)
 		{
-			if (secret[i] == letter)
+			for (int c = 0; c < guess.length(); c++)
 			{
-				partialWord[i] = letter;
-				found = true;
+				char secretletter = secret[i];
+				//std::cout << secretletter << std::endl;
+				std::cout << secret[i] << std::endl;
+				char guessletter = guess[c];
+				//std::cout << guessletter << std::endl;
+				std::cout << guess[c] << std::endl;
+				//if (guessletter == secretletter)
+				if (guess[c] == secret[i])
+				{
+					l++;
+					if (l > 4)
+					{
+						std::cout << "You win! The word was " << secret << std::endl;
+						found = true;
+					}
+				}
+				
 			}
-
 		}
+
+		//}
+		/*
+		if (l = 5)
+		{
+			std::cout << "You win! The word was " << secret << std::endl;
+			found = true;
+			//break;
+		}
+		
+		*/
 		if (!found)
 		{
 			lives--;
 			if (lives <= 0)
 			{
 				std::cout << "You lose :( The word was " << secret << std::endl;
-				gameOver = true;
+				//gameOver = true;
 				//break;
 			}
 		}
-
-		if (partialWord == secret)
-		{
-			std::cout << "You win! The word was " << secret << std::endl;
-			//break;
-		}
 	}
-
 
     return 0;
 }
