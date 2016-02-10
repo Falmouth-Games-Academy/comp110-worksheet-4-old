745df8ebddea78c0b049dddaf05813c6524e77fc
ValrynMilly <Emiljanokurbiba@gmail.com>
2016-02-10 03:09:59 +0000

    This ones tricky.

--- PartB_Mandelbrot/Mandelbrot.cpp
+++ PartB_Mandelbrot/Mandelbrot.cpp
@@ -1,62 +1,29 @@
-// PartC_Mandelbrot.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
-#include "colours.h"
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <complex>
 
 int main()
 {
-	// Initialise the image
-	CImg<unsigned char> image(800, 800, 1, 3, 0);
+  enum { Width = 500, Height = 500 };
+  if (SDL_Init(SDL_INIT_VIDEO) != 0)
+  {
+    std::cerr << "SDL_Init(SDL_INIT_VIDEO)\n";
+    return -1;
+  }
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  SDL_CreateWindowAndRenderer(Width, Height, SDL_WINDOW_OPENGL | SDL_WINDOW_BORDERLESS, &window, &renderer);
+  SDL_SetWindowPosition(window, 65, 126);
 
-	// Display the image
-	CImgDisplay display(image, "Mandelbrot Set");
+  enum { PaletteSize = 32 };
+  int palette[PaletteSize + 1][3];
+  for (int i = 0; i <= PaletteSize; ++i)
+  {
+    palette[i][0] = i < 2 * PaletteSize / 3 ? i * 255 * 3 / (2 * PaletteSize) : 255;
+    palette[i][1] = i < PaletteSize / 3 ? 0 : (i - PaletteSize / 3) * 255 * 3 / (2 * PaletteSize);
+    palette[i][2] = 0;
+  }
 
-	// Cutoff for iteration
-	const int maxIters = 200;
-
-	// Initialise the colour palette
-	std::vector<Colour> palette = getRainbow(maxIters);
-
-	const double minX = -2, maxX = 1, minY = -1.5, maxY = 1.5;
-
-	// Generate the image
-	for (int pixelY = 0; pixelY < image.height(); pixelY++)
-	{
-		// TODO: Map the y coordinate into the range minY to maxY
-		//double y0 =
-
-		for (int pixelX = 0; pixelX < image.width(); pixelX++)
-		{
-			// TODO: Map the x coordinate into the range minX to maxX
-			//double x0 =
-
-			// TODO: implement the algorithm to colour a single pixel (x0, y0) of the Mandelbrot set fractal
-			Colour colour = { 0,0,0 };
-
-			// Write the pixel
-			image(pixelX, pixelY, 0, 0) = colour.r;
-			image(pixelX, pixelY, 0, 1) = colour.g;
-			image(pixelX, pixelY, 0, 2) = colour.b;
-		}
-
-		// Uncomment this line to redisplay the image after each row is generated
-		//display.display(image);
-	}
-
-	// Display the complete image
-	display.display(image);
-
-	// Uncomment this line to save the image to disk
-	//image.save_bmp("mandelbrot.bmp");
-
-	// Wait for the window to be closed
-	while (!display.is_closed())
-	{
-		display.wait();
-	}
-
-	return 0;
-}
-
+  for (int y = 0; y < Height; ++y)
+    for (int x = 0; x < Width; ++x)
 
