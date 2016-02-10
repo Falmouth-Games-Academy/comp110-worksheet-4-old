729a6a884048ba67a9cd90d5cd89c75dcf9ebf10
NecroReindeer <necroreindeer@gmail.com>
2016-02-10 09:49:45 +0000

    Added temporary function for generating julia set

--- PartB_Mandelbrot/Mandelbrot.cpp
+++ PartB_Mandelbrot/Mandelbrot.cpp
@@ -4,7 +4,10 @@
 #include "stdafx.h"
 #include "colours.h"
 
-void generate(CImg<unsigned char> &image)
+
+
+
+/*void generate(CImg<unsigned char> &image)
 {
 	const int maxIters = 200;
 
@@ -27,7 +30,7 @@ void generate(CImg<unsigned char> &image)
 			double memY = y0;
 			unsigned char i = 0;
 
-			while ((pow(memX, 2) + pow(memY, 2)) < 4 && i < maxIters)
+			while (memX*memX + memY*memY < 4 && i < maxIters)
 			{
 				double nextX = pow(memX, 2) - pow(memY, 2) + x0;
 				double nextY = (2 * memX * memY) + y0;
@@ -53,7 +56,59 @@ void generate(CImg<unsigned char> &image)
 			image(pixelX, pixelY, 0, 2) = colour.b;
 		}
 	}
+}*/
+
+void generate(CImg<unsigned char> &image)
+{
+	const int maxIters = 200;
+
+	// Initialise the colour palette
+	std::vector<Colour> palette = getRainbow(maxIters);
+
+	const double minX = -1.5, maxX = 1.5, minY = -1.5, maxY = 1.5;
+
+	// Generate the image
+	for (int pixelY = 0; pixelY < image.height(); pixelY++)
+	{
+		double y0 = (double(pixelY) / image.height()) * (maxY - minY) + minY;
+
+		for (int pixelX = 0; pixelX < image.width(); pixelX++)
+		{
+			double x0 = (double(pixelX) / image.width()) * (maxX - minX) + minX;
+
+			// Initialise first term of the sequence
+			double memX = x0;
+			double memY = y0;
+			unsigned char i = 0;
+
+			while (memX*memX + memY*memY < 4 && i < maxIters)
+			{
+				double nextX = pow(memX, 2) - pow(memY, 2) + 0.279;
+				double nextY = (2 * memX * memY);
+
+				// Used checking this iteration and calculating the next
+				memX = nextX;
+				memY = nextY;
+
+				// To keep track of position in sequence
+				i++;
+			}
+
+			Colour colour;
+
+			if (i >= maxIters)
+				colour = { 0, 0, 0 };
+			else
+				colour = palette[i];
+
+			// Write the pixel
+			image(pixelX, pixelY, 0, 0) = colour.r;
+			image(pixelX, pixelY, 0, 1) = colour.g;
+			image(pixelX, pixelY, 0, 2) = colour.b;
+		}
+	}
 }
+
 
 int main()
 {
