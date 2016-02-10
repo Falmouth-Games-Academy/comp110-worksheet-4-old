1ea859f9f0a7784b4bc8553e9f60895c00017387
Sam Wills <sw180283@falmouth.ac.uk>
2016-02-10 09:56:00 +0000

    Commented code for Mandelbrot

--- PartB_Mandelbrot_Second_Attempt/Mandelbrot.cpp
+++ PartB_Mandelbrot_Second_Attempt/Mandelbrot.cpp
@@ -4,22 +4,6 @@
 #include "stdafx.h"
 #include "colours.h"
 
-double getMandelBrot(double cr, double ci, double maxIters)
-{
-	int i = 0;
-	double zr = 0.0, zi = 0.0;
-	while (i < maxIters && pow(zr,2) + pow(zi, 2) < 4.0)
-	{
-		double temp = pow(zr, 2) - pow(zi, 2) + cr;
-		zi = 2.0 * zr * zi + ci;
-		zr = temp;
-		i++;
-	}
-
-	return i;
-}
-
-
 int main()
 {
 	// Initialise the image
@@ -36,29 +20,19 @@ int main()
 
 	const double minX = -2, maxX = 1, minY = -1.5, maxY = 1.5;
 	int black = 0x000000, white = 0xFFFFFF;
-
-	/*
-	for (int pixelX = 0; pixelX < image.height(); pixelX++)
-	{
-
-	for (int pixelY = 0; pixelY < image.width(); pixelY)
-	{
-	*/
-	//double cr = (pixelY - image.width() / 2.0) * 4.0 / image.width();
-	//double ci = (pixelX - image.height() / 2.0) * 4.0 / image.width();
 	
 	// Generate the image
 	for (double pixelY = 0; pixelY < image.height(); pixelY++)
 	{
-		// TODO: Map the y coordinate into the range minY to maxY
+		// Map the y coordinate into the range minY to maxY
 		double y0 = (pixelY / image.height()) * (maxY - minY) + minY;
 
 		for (double pixelX = 0; pixelX < image.width(); pixelX++)
 		{
-			// TODO: Map the x coordinate into the range minX to maxX
+			// Map the x coordinate into the range minX to maxX
 			double x0 = (double(pixelX) / image.width()) * (maxX - minX) + minX;
 
-			// TODO: implement the algorithm to colour a single pixel (x0, y0) of the Mandelbrot set fractal
+			// Get the exit iteration for a single pixel (x0, y0) of the Mandelbrot set fractal
 			double x = 0, y = 0;
 
 			int i = 0;
@@ -70,18 +44,17 @@ int main()
 				i++;
 			} 
 			
+			// Set the colour for each pixel using the palette
 			Colour colour;
 
-			if (i < maxIters) colour = palette[i]; //colour = { static_cast<unsigned char>(pixelY), static_cast<unsigned char>(pixelX), white };
+			if (i < maxIters) colour = palette[i];
 			
-			else colour = { 0,0,0 };  //colour = { static_cast<unsigned char>(pixelY), static_cast<unsigned char>(pixelX), black };
+			else colour = { 0,0,0 };
 
 			// Write the pixel
 			image(pixelX, pixelY, 0, 0) = colour.r;
 			image(pixelX, pixelY, 0, 1) = colour.g;
 			image(pixelX, pixelY, 0, 2) = colour.b;
-				//}
-			//}
 
 		}
 
