14c7e4bcc120f9842e618846d5997d7eb82d210e
ValrynMilly <Emiljanokurbiba@gmail.com>
2016-02-10 02:39:53 +0000

    Forgot to add Comments on code oops, no worries that’s what I just did :)

--- PartA_TerminalHacking/Terminal.cpp
+++ PartA_TerminalHacking/Terminal.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-#include <windows.h>
+#include <windows.h> 
+//Imports above are needed to make this program work
 using namespace std;
 
 
@@ -10,16 +11,16 @@ using namespace std;
 int main()
 {
 	
-		SetConsoleTitle("Terminal Station");
-		HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-		SetConsoleTextAttribute(h, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+		SetConsoleTitle("Terminal Station"); //This sets the title of the program
+		HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE); //declaring a handlee so programmer can change appearance of game
+		SetConsoleTextAttribute(h, FOREGROUND_GREEN | FOREGROUND_INTENSITY); //Changin the color of text
 		
-	int Guesses = 5;
-	string Guess;
-	string Answer;
-	int i;
+	int Guesses = 5; //Declaring the ammount of Guesses function 
+	string Guess; //declared users guess as string since its letters only
+	string Answer; //The Predefined answer is declared
+	int i; //This is needed for the below loop
 
-	Answer = "BORED";
+	Answer = "BORED"; //Answer
 
 	cout << "MILLYCO INDUSTRIES (TM) TERMLINK PROTOCOL" << endl;
 	cout << "PASSWORD REQUIRED - ENTER NOW" << endl;
@@ -35,9 +36,11 @@ int main()
 	cout << "LLING/..?:AOIWD" << endl;
 	cout << "BORED-01*23..>;" << endl;
 
-do { if (Guesses > 0) {
+	//Text above is what is displayed on launch of program so user understands
+
+do { if (Guesses > 0) { //The Loop for likeness of characters
 cout << " > ";
-cin >> Guess; 
+cin >> Guess; //user inputting function
 if (Guess != "BORED") {
 if (Guess == "SNEAKING" || Guess == "THROWING" || Guess == "SMELLING" || Guess == "LOCKPICK" ) { 
 cout << "Incorrect! " << "1/5 Characters Right" << endl;}
@@ -49,6 +52,7 @@ for (i=0; i<Guesses; i++) {
 cout << "?"; } cout << endl;
 } while (Guess != "BORED" && Guesses != 0); 
 
+//loop ran after users guess is inputted determing if Pass or Failed
 if ( Guess == "BORED" ) {
 cout << "ACCESS GRANTED" << endl; }
 else {
